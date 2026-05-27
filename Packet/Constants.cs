f38468c29f51cfe2c25231190fa159c144f90382
struuct <134949919+struuct@users.noreply.github.com>
2026-05-26 23:01:05 -0500

    bump version again

--- Packet/Constants.cs
+++ Packet/Constants.cs
@@ -2,7 +2,7 @@ namespace Packet;
 
 internal static class Constants
 {
-    public const string Version = "1.0.2";
+    public const string Version = "1.0.3";
     public const string ModName = "Packet";
     public const string Guid = "struct.packet";
     
