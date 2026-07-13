812723fd9d0189943bda895dcc4dacc1789b5f40
struuct <134949919+struuct@users.noreply.github.com>
2026-07-12 19:53:40 -0700

    i am an idiot

--- Packet/Constants.cs
+++ Packet/Constants.cs
@@ -2,10 +2,10 @@ namespace Packet;
 
 internal static class Constants
 {
-    public const string Version = "1.0.3";
+    public const string Version = "1.0.4";
     public const string ModName = "Packet";
     public const string Guid = "struct.packet";
-    
+
     public const string BackendUrl = "https://packet.struct.fyi";
     public const string BackendProtocolVersion = "1";
 }
